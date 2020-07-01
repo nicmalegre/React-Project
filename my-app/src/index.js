4ea59ab1022b9457d446f7ed397bc2b53b54b5eb
Nicolas <nikoalegre@hotmail.com>
2020-07-01 20:52:07 -0300

    Funciona el juego con 2 jugadores: X y O. Falta agregar el historial

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,23 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
+function Square(props) {        
       return (
-        <button className="square">
-          {this.props.value}
+        <button className="square" 
+        onClick={props.onClick}
+        >
+          {props.value}
         </button>
       );
-    }
   }
   
 class Board extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { //Esto es como pasarle this.state.squares donde ese squares 
+                        //esta compuesto por el arreglo de 9 elementos con null
+            squares: Array(9).fill(null),
+            xIsNext:true, //Para controlar si poner una X o una O
+        };
+    }
+
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O'; //Preguntamos si xIsNext es true o false y dependiendo de eso le ponemos X o O
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext, //Si estaba en True pasa a False o viceversa
+        });
+    }
+    
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square 
+            value={this.state.squares[i]}
+            onClick = {() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
         <div>
